1ad355455ce8df20c31640502f64da93b8bd769f
AnaChamba <abm.chamba@gmail.com>
2020-07-30 05:38:40 -0500

    Clase Carpintero.cs

---
+++ ProfesionesInterface/Carpintero.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ProfesionesInterface
+{
+    public class Carpintero
+    {
+        public string Diseñar()
+        {
+            return "Estoy diseñando";
+        }
+        public string Medir()
+        {
+            return "Estoy midiendo";
+        }       
+        public string Serruchar()
+        {
+            return "Estoy serruchando";
+        }
+        public string Clavar()
+        {
+            return "Estoy clavando";
+        }
+        public string Lijar()
+        {
+            return "Estoy linjando";
+        }
+        public string Lacar()
+        {
+            return "Estoy lacando";
+        }
+
+        public class CarpinteroModerno
+        {
+            public string Diseñar()
+            {
+                return "Estoy diseñando con láser";
+            }
+            public string Medir()
+            {
+                return "Estoy midiendo con ";
+            }
+            public string Serruchar()
+            {
+                return "Estoy serruchando";
+            }
+            public string Clavar()
+            {
+                return "Estoy clavando";
+            }
+            public string Lijar()
+            {
+                return "Estoy linjando";
+            }
+            public string Lacar()
+            {
+                return "Estoy lacando";
+            }
+        }
+}
